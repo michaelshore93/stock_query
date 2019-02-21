e7c50f224889c8b724555827ce5ceee2c4002757
michaelshore93 <33969704+michaelshore93@users.noreply.github.com>
2019-02-20 23:22:31 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
-"""This program uses Quandl's API for Microsoft's stock data, putting that data
-    in a JSON format. Then, that JSON is transferred to a SQLite database via
-    SQLAlchemy ORM. From there, the data is parsed into Excel, where it is
-    utilized to plot charts to provide insights on close price, high/low
-    spreads, open/clode deltas, and trading volumes."""
+"""Module to run to program.
+
+Operates a series of queries from JSON to SQLite to Excel.
+"""
+import stock_query
 
 import stock_query
 
 if __name__ == "__main__":
-    stock_query.JSON_Connection.connect_json()
-    stock_query.DB_Session.json_to_db()
-    stock_query.Workbook.closeprice()
-    stock_query.Workbook.open_close_delta()
+    print("""I only have a free API (not paying $35/mo for premium, so \
+you can only use my key for certain stocks. \
+Apple (AAPL), Microsoft (MSFT), and Goldman Sachs (GS) are some \
+that work.\n\nIf you want to check, type \
+https://www.quandl.com/api/v3/datasets/EOD/{STOCK TICKER HERE}.json?api_key=EPaRw4wZxHqisMT9gMdX \
+into your browser.""")
+   stock_query.JSON_Connection.connect_json()
+   stock_query.DB_Session.json_to_db()
+   stock_query.Workbook.closeprice()
+   stock_query.Workbook.open_close_delta()
     print("NOTE: All prices are adjusted using CRSP Methodology")
     print("http://www.crsp.com/products/documentation/crsp-calculations")
