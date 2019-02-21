da461a6470894780419fc735b0d6ec60f205d30b
michaelshore93 <33969704+michaelshore93@users.noreply.github.com>
2019-02-20 23:49:41 -0500

    Update run.py

--- run.py
+++ run.py
@@ -12,9 +12,9 @@ Apple (AAPL), Microsoft (MSFT), and Goldman Sachs (GS) are some \
 that work.\n\nIf you want to check, type \
 https://www.quandl.com/api/v3/datasets/EOD/{STOCK TICKER HERE}.json?api_key=EPaRw4wZxHqisMT9gMdX \
 into your browser.""")
-   stock_query.JSON_Connection.connect_json()
-   stock_query.DB_Session.json_to_db()
-   stock_query.Workbook.closeprice()
-   stock_query.Workbook.open_close_delta()
+    stock_query.JSON_Connection.connect_json()
+    stock_query.DB_Session.json_to_db()
+    stock_query.Workbook.closeprice()
+    stock_query.Workbook.open_close_delta()
     print("NOTE: All prices are adjusted using CRSP Methodology")
     print("http://www.crsp.com/products/documentation/crsp-calculations")
