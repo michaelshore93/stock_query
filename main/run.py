c360a091e16cc152309c428832ff36744e244de7
michaelshore93 <33969704+michaelshore93@users.noreply.github.com>
2019-02-21 19:01:13 -0500

    Update run.py

--- main/run.py
+++ main/run.py
@@ -6,12 +6,6 @@ Operates a series of queries from JSON to SQLite to Excel.
 import stock_query
 
 if __name__ == "__main__":
-    print("""I only have a free API (not paying $35/mo for premium, so \
-you can only use my key for certain stocks. \
-Apple (AAPL), Microsoft (MSFT), and Goldman Sachs (GS) are some \
-that work.\n\nIf you want to check, type \
-https://www.quandl.com/api/v3/datasets/EOD/{STOCK TICKER HERE}.json?api_key=EPaRw4wZxHqisMT9gMdX \
-into your browser.""")
     stock_query.JSON_Connection.connect_json()
     stock_query.DB_Session.json_to_db()
     stock_query.Workbook.closeprice()
