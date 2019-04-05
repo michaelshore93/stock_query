234d4535d4563a75df978c47bbfcbf6038cf18a4
michaelshore93 <33969704+michaelshore93@users.noreply.github.com>
2019-04-05 18:35:02 -0400

    fixed error

--- main/stock_query.py
+++ main/stock_query.py
@@ -151,7 +151,7 @@ class Workbook:
         line.width = 35
         line.legend = None
         ws.add_chart(line, "D10")
-        # Workbook.wb.save(Workbook.wbook_name)
+        Workbook.wb.save(Workbook.wbook_name)
 
     def open_close_delta():
         """Creates a sheet for Open/Close delta."""
