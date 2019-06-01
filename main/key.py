f6922572189333af86a6c88f483a7c892e351884
michaelshore93 <33969704+michaelshore93@users.noreply.github.com>
2019-06-01 13:45:20 -0400

    Change key.

--- main/key.py
+++ main/key.py
@@ -1,2 +1,2 @@
 # You can get a free API Key here: https://www.quandl.com/tools/api. I put a free API key here for ease of use, but this is not recommended for security reasons.
-api_key = "AjdDyUMTS3znFrrNsCJh"
+api_key = 'AjdDyUMTS3znFrrNsCJh'
